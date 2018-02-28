d214f51134cd535add3feed2d2d732d258c4581d
yel <yel@sucsoft.com>
2018-02-28 16:35:28 +0800

    1.4.0

--- ng-lazy-load.js
+++ ng-lazy-load.js
@@ -2,7 +2,7 @@
 
     function factory(angular) {
 
-        var VERSION = '1.3.0';
+        var VERSION = '1.4.0';
 
         // Support require.js, sea.js, system.js
         var amdRequire = (function () {
